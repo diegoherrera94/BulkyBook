45aa53a0c6ca8524807b077e4b51463e2d310f75
Diego Herrera <diegogaston4@gmail.com>
2022-11-25 00:04:52 -0300

    details page created

--- EcommerceWeb/Areas/Customer/Controllers/HomeController.cs
+++ EcommerceWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Ecommerce.Models;
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Models;
+using Ecommerce.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +10,34 @@ namespace EcommerceWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll("Category,CoverType");
+            return View(products);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.Product.GetFirstOrDefault(p => p.Id == id, "Category,CoverType"),
+                Count = 1
+            };
+
+            return View(cart);
         }
 
         public IActionResult Privacy()
